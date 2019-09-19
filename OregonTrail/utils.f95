d1a77d3088fee4105e1898250f749db8e9ef67f5
stephen_johnston <stephenkingjohnston@gmail.com>
2019-09-18 22:08:01 -0700

    Add link to to_upper solution. Change casing from Result to result.

--- OregonTrail/utils.f95
+++ OregonTrail/utils.f95
@@ -4,8 +4,9 @@ module utils
     contains
         !   ==============================
         !   Changes a string to upper case
+        !   Source: https://stackoverflow.com/a/10807829
         !   ==============================
-        pure function to_upper (str) Result (string)
+        pure function to_upper (str) result (string)
             implicit none
             character(*), intent(In) :: str
             character(len(str))      :: string
